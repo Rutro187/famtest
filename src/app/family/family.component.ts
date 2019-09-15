4e2dd988ba139d7441618d5167f4e4658011dc0d
Rutro187 <49966402+Rutro187@users.noreply.github.com>
2019-09-15 18:26:03 -0500

    AddedAllTheFirebase

--- src/app/family/family.component.ts
+++ src/app/family/family.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import {FamilyService} from '../family.service';
+
+export interface Status {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-family',
@@ -6,10 +14,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./family.component.scss']
 })
 export class FamilyComponent implements OnInit {
+  familyForm: FormGroup;
+  statuses: Status[] = [
+    {value: 'both-0', viewValue: 'Both'},
+    {value: 'active-1', viewValue: 'Active'},
+    {value: 'closed-2', viewValue: 'Closed'}
 
-  constructor() { }
+  ];
+
+  constructor(private formBuilder: FormBuilder, private familyService: FamilyService) { }
+
+  get familyid() {
+    return this.familyForm.get('familyid');
+  }
+
+  get CFN() {
+    return this.familyForm.get('CFN');
+  }
+  get familyName() {
+    return this.familyForm.get('familyName');
+  }
+  get CLN() {
+    return this.familyForm.get('CLN');
+  }
+  get firstName() {
+    return this.familyForm.get('firstName');
+  }
+  get lccStaff() {
+    return this.familyForm.get('lccStaff');
+  }
+  get lastName() {
+    return this.familyForm.get('lastName');
+  }
+
 
   ngOnInit() {
+    this.familyForm = this.formBuilder.group({
+      familyid: [''],
+      CFN: [''],
+      familyName: [''],
+      CLN: [''],
+      firstName: [''],
+      lccStaff: [''],
+      lastname: [''],
+
+
+    });
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBDuVhhatcfhJWqTcekfWx7zsqvVWDUSrM',
+    authDomain: 'lcpoc-252804.firebaseapp.com',
+    databaseURL: 'https://lcpoc-252804.firebaseio.com',
+    projectId: 'lcpoc-252804',
+    storageBucket: 'lcpoc-252804.appspot.com',
+    messagingSenderId: '872407211417',
+    appId: '1:872407211417:web:3b384d0ed92cfa0605c708'
+  }
+
 };
