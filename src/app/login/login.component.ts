37d82ce59e7a803e44b0ce08280337f4c9a724a0
Rutro187 <mjohnson@esomaha.com>
2019-09-16 11:24:47 -0500

    Updated Login/userman Componets

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService} from '../authentication.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,10 +10,22 @@ import { AuthenticationService} from '../authentication.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService) { }
 
+    get email(){
+      return this.loginForm.get('email')
+    }
+
+    get password(){
+      return this.loginForm.get('password')
+    }
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['',  Validators.required],
+    })
   }
 
 }
--- src/app/userman/userman.component.ts
+++ src/app/userman/userman.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService} from '../authentication.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-userman',
@@ -7,17 +9,22 @@ import { AuthenticationService} from '../authentication.service';
   styleUrls: ['./userman.component.scss']
 })
 export class UsermanComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor(public authenticationService: AuthenticationService) { }
+  constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService) { }
 
-  signUp() {
-    this.authenticationService.SignUp(this.email, this.password);
-    this.email = '';
-    this.password = '';
-  }
+  // signUp() {
+  //   this.authenticationService.SignUp(this.email, this.password);
+  //   this.email = '';
+  //   this.password = '';
+  // }
 
 
   ngOnInit() {
+    this.signupForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['',  Validators.required],
+    })
   }
 
 }
