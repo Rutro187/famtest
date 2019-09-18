df21800f7b6756fc129139a3c87ac06298bcef56
Rutro187 <mjohnson@esomaha.com>
2019-09-18 12:39:04 -0500

    familycrud

--- src/app/Models/Interfaces/Employment.ts
+++ src/app/Models/Interfaces/Employment.ts
@@ -8,5 +8,4 @@ export const employments: Employment[] = [
   {value: '2', viewValue: 'Unemployed'},
   {value: '3', viewValue: 'Prefer not to say'},
   {value: '4', viewValue: 'Other'},
-  
-];
+  ];
--- src/app/familycrud/familycrud.component.ts
+++ src/app/familycrud/familycrud.component.ts
@@ -20,31 +20,93 @@ export class FamilycrudComponent implements OnInit {
 
 
   get familyid() {
-    return this.familyForm.get('familyid');
+    return this.familyAddForm.get('familyid');
   }
 
-  get CFN() {
-    return this.familyForm.get('CFN');
+  get DOB() {
+    return this.familyAddForm.get('DOB');
   }
   get familyName() {
-    return this.familyForm.get('familyName');
+    return this.familyAddForm.get('familyName');
   }
-  get CLN() {
-    return this.familyForm.get('CLN');
+  get marital() {
+    return this.familyAddForm.get('marital');
+  }
+  get otherMarital() {
+    return this.familyAddForm.get('otherMarital');
   }
   get firstName() {
-    return this.familyForm.get('firstName');
-  }
-  get lccStaff() {
-    return this.familyForm.get('lccStaff');
+    return this.familyAddForm.get('firstName');
   }
   get lastName() {
-    return this.familyForm.get('lastName');
+    return this.familyAddForm.get('lastName');
+  }
+  get race() {
+    return this.familyAddForm.get('race');
+  }
+  get sex() {
+    return this.familyAddForm.get('sex');
+  }
+  get otherRace() {
+    return this.familyAddForm.get('otherRace');
+  }
+  get address1() {
+    return this.familyAddForm.get('address1');
+  }
+  get yis() {
+    return this.familyAddForm.get('yis');
+  }
+  get address2() {
+    return this.familyAddForm.get('address2');
+  }
+  get noy() {
+    return this.familyAddForm.get('noy');
+  }
+  get city() {
+    return this.familyAddForm.get('city');
+  }
+  get hg() {
+    return this.familyAddForm.get('hg');
+  }
+  get zip() {
+    return this.familyAddForm.get('zip');
+  }
+  get otherGrade() {
+    return this.familyAddForm.get('otherGrade');
+  }
+  get phone1() {
+    return this.familyAddForm.get('phone1');
+  }
+  get type1() {
+    return this.familyAddForm.get('type1');
+  }
+  get employment() {
+    return this.familyAddForm.get('employment');
+  }
+  get phone2() {
+    return this.familyAddForm.get('phone2');
+  }
+  get type2() {
+    return this.familyAddForm.get('type2');
+  }
+  get otherEmploy() {
+    return this.familyAddForm.get('otherGrade');
+  }
+  get relation() {
+    return this.familyAddForm.get('relation');
+  }
+  get email() {
+    return this.familyAddForm.get('email');
+  }
+  get email2() {
+    return this.familyAddForm.get('email2');
+  }
+  get otherRelation() {
+    return this.familyAddForm.get('otherRelation');
   }
 
-
   ngOnInit() {
-    this.familyForm = this.formBuilder.group({
+    this.familyAddForm = this.formBuilder.group({
       familyid: [''],
       CFN: [''],
       familyName: [''],
