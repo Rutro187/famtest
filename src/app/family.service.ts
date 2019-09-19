929e0c9801ad706c61e76463b0e3635fd63fe2dd
Rutro187 <49966402+Rutro187@users.noreply.github.com>
2019-09-19 10:32:56 -0500

    f

--- src/app/family.service.ts
+++ src/app/family.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class FamilyService {
 
   constructor(
-    // private firestore: AngularFirestore
+    private firestore: AngularFirestore
   ) { }
 
 
-// addFamily(record) {
-//   return this.firestore.collection('Family').add(record);
-//   }
+addFamily(record) {
+  return this.firestore.collection('Family').add(record);
+  }
 // searchFamily(record) {
 //   return this.firestore.collection('Family').snapshotChanges(record);
 // }
